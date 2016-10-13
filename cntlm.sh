76a02b3d7f3db5b9397be694831979a6968f27ee
John Burns <jzburns@gmail.com>
2016-10-13 10:46:34 +0100

    My Diary

---
+++ cntlm.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+
+#ps -ef -> Prints a snapshot of every process in the system
+
+#grep cntlm -> searches for the text cntlm
+
+#Where does the text come from? ps -ef |
+
+#This is now a new command
+
+#Searches all the running processes looking for the cntlm process
+
+
+
+ps -ef | grep cntlm
---
+++ pipe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#ps -ef -> Prints a snapshot of every process in the system
+
+#grep mysql -> searches for the text mysql
+
+#Where does the text come from? ps -ef |
+
+#This is now a new command
+
+#Searches all the running processes looking for the mysql process
+
+ps -ef | grep mysql
